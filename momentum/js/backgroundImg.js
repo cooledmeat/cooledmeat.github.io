02925f444c33969dfade690c9d913c778455dcfb
cooledmeat <dnglglglgl11@gmail.com>
2021-07-12 02:32:23 +0900

    login

--- momentum/js/backgroundImg.js
+++ momentum/js/backgroundImg.js
@@ -1,7 +1,15 @@
-const images = ["https://images.unsplash.com/photo-1625976927996-a50b023a23d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1715&q=80"];
+const images = [
+  "https://images.unsplash.com/photo-1625976927996-a50b023a23d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1715&q=80",
+  "https://images.unsplash.com/photo-1625990943295-6b06dbca7948?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80",
+  "https://images.unsplash.com/photo-1626013607276-6fad3985a1a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+  "https://images.unsplash.com/photo-1625959007780-6531d85798c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+  "https://images.unsplash.com/photo-1625918328402-327e34637d41?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+];
 
-const  bgimg = document.createElement("img");
+const bgimg = document.createElement("img");
 
-bgimg.src = images[0];
+const ranImg = images[Math.floor(Math.random() * images.length)];
+
+bgimg.src = `${ranImg}`;
 
 document.body.appendChild(bgimg);
--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,1 +1,14 @@
-const date = new Date();
+const clock = document.getElementById("clock");
+const titleClock = document.getElementById("titleClock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  titleClock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
