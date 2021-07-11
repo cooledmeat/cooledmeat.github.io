e26622fa1a055f822d4c44b3cf3a72c0f0b17a75
cooledmeat <dnglglglgl11@gmail.com>
2021-07-12 01:00:38 +0900

    img

---
+++ momentum/js/backgroundImg.js
@@ -0,0 +1,7 @@
+const images = ["https://images.unsplash.com/photo-1625976927996-a50b023a23d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1715&q=80"];
+
+const  bgimg = document.createElement("img");
+
+bgimg.src = images[0];
+
+document.body.appendChild(bgimg);
