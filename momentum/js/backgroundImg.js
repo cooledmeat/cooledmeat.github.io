bc2cef11c0caad75fe80140ece74b6e4abf53a7a
cooledmeat <dnglglglgl11@gmail.com>
2021-07-20 03:52:26 +0900

    css

--- momentum/js/backgroundImg.js
+++ momentum/js/backgroundImg.js
@@ -6,10 +6,11 @@ const images = [
   "https://images.unsplash.com/photo-1625918328402-327e34637d41?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
 ];
 
+const backImg = document.querySelector(".backgroundImg");
 const bgimg = document.createElement("img");
 
 const ranImg = images[Math.floor(Math.random() * images.length)];
 
 bgimg.src = `${ranImg}`;
 
-document.body.appendChild(bgimg);
+backImg.appendChild(bgimg);
